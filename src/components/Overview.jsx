509672ba240eaa198a0c9f9a8516e4cb2815c0d6
shubham-am-i <patilshubham2k21@gmail.com>
2022-07-19 10:40:15 +0530

    fix UI on mobile devices

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -3,6 +3,7 @@ import CreateHabit from './CreateHabit'
 import { Row, Col, ListGroup, Button } from 'react-bootstrap'
 import { deleteHabit } from '../features/habitsSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import './overview.css'
 
 const Overview = () => {
   const [modalShow, setModalShow] = React.useState(false)
@@ -19,23 +20,23 @@ const Overview = () => {
   return (
     <>
       <Row>
-        <Col sm={2} md={2}>
+        <Col md={2} className='onMobile'>
           <i className='fa-regular fa-calendar-minus'></i>
         </Col>
-        <Col>
+        <Col md={4} className='onMobile'>
           <h4>Habits</h4>
         </Col>
       </Row>
       <ListGroup>
         {habits.map((habit, index) => (
-          <ListGroup.Item key={index} className='gradient'>
+          <ListGroup.Item key={index} className='gradient mb-1 rounded habit-container'>
             <Row>
-              <Col md={2}>
+              <Col md={2} className='icons'>
                 {' '}
                 <i className='fa-solid fa-feather-pointed me-3'></i>
               </Col>
-              <Col md={8}>{habit.title}</Col>
-              <Col md={1}>
+              <Col md={8} className='habit-title'>{habit.title}</Col>
+              <Col md={1} className='icons'>
                 <i
                   className='fa-solid fa-trash'
                   style={{ fontSize: '12px' }}
@@ -47,13 +48,14 @@ const Overview = () => {
       </ListGroup>
 
       <Row>
-        <Col></Col>
-        <Col md={9}>
+        {/* <Col></Col> */}
+        <Col className='d-flex justify-content-end'>
           <Button
             type='button'
-            className='bg-primary mt-3 ml-5'
+            className='bg-primary mt-3 mobile'
             onClick={() => setModalShow(true)}>
-            <i className='fa-solid fa-circle-plus'></i> &nbsp;&nbsp;Create New Habit
+
+            <i className='fa-solid fa-circle-plus'></i> &nbsp;&nbsp; New Habit
           </Button>
           <CreateHabit show={modalShow} onHide={() => setModalShow(false)} />
         </Col>
